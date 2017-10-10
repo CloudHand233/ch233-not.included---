999d579a74c8e8f62f0223ec3e413b73d9e9f9bc
ch233 <cloudhand233@gmail.com>
2017-10-11 03:27:31 +0800

    change theme

--- src/js/_cover_animate.js
+++ src/js/_cover_animate.js
@@ -1,11 +1,27 @@
+//cover_bg.png animate
+// function coverAnimate() {
+//     $("#coverImg").css({"top": "0px","left": "0px"});
+//     $("#coverImg").animate({
+//         top: '-96px',
+//     },600);
+//     $("#coverImg").animate({
+//         left: '-167px',
+//     },600);
+// }
+
+//cover_bg.gif animate
 function coverAnimate() {
     $("#coverImg").css({"top": "0px","left": "0px"});
     $("#coverImg").animate({
-        top: '-96px',
-    },600);
+        top: '-687px',
+        left: '-1010px'
+    },7000);
     $("#coverImg").animate({
-        left: '-167px',
-    },600);
+        left: '0'
+    },2000);
+    $("#coverImg").animate({
+        left: '-1010px'
+    },1400);
 }
-
-setInterval(coverAnimate,5500);
+coverAnimate();
+setInterval(coverAnimate,15000);
