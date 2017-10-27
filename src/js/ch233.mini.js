126d03c86510b1369ef6972c70c0267501c5ebe5
ch233 <cloudhand233@gmail.com>
2017-10-27 21:36:31 +0800

    guide js done

--- src/js/ch233.mini.js
+++ src/js/ch233.mini.js
@@ -1,5 +1,5 @@
  // * Author: 云手
- //  * GitHub: https://github.com/CloudHand233/ch233-smile---
+ //  * GitHub: https://github.com/CloudHand233/ch233-not.included---
  //  * LastUpdate: 2017.Aug.25th
  //  * Version: 3.0.0
 
